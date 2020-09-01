be1bca815421a917eeea41883a49ff58052737bf
fewizz <hazeevaidar@gmail.com>
2020-09-01 14:50:11 +0300

    static assertions, don't use invalid_t, int-\>std::size_t

--- include/tv/type_vector.hpp
+++ include/tv/type_vector.hpp
@@ -1,75 +1,94 @@
 #pragma once
 
+#include <bits/c++config.h>
+#include <iterator>
+#include <streambuf>
 #include <type_traits>
+#include <limits>
 
 namespace tv {
 
 template<class... Ts>
-class type_vector {
-	struct invalid_t{};
-public:
+struct type_vector {
+	static constexpr std::size_t size_v = sizeof...(Ts);
+	static constexpr bool empty_v = size_v == 0;
+	static constexpr std::size_t not_found_v = std::numeric_limits<std::size_t>::max();
 
-	template<int index>
+	template<std::size_t index>
 	struct at {
-		template<int index0, class... Ts0>
+		static_assert(!empty_v, "type vector is empty");
+		static_assert(index >= 0 && index < size_v, "index out of bounds");
+		
+		template<std::size_t index0, class Head, class... Tail>
 		struct of {
-			using type = invalid_t;
+			using type = typename of<index0+1, Tail...>::type;
 		};
 
-		template<class...Unused>
-		struct of<-1, Unused...> {
-			using type = invalid_t;
-		};
-		
-		template<int index0, class Head, class... Tail>
-		struct of<index0, Head, Tail...> {
-			using type =
-				std::conditional_t<
-					index0==index,
-					Head,
-					typename of<index0+1, Tail...>::type
-				>;
+		template<class T, class... Tail>
+		struct of<index, T, Tail...> {
+			using type = T;
 		};
 
 		using type = typename of<0, Ts...>::type;
 	};
 
-	template<int index>
+	template<std::size_t index>
 	using at_t = typename at<index>::type;
 	
+	template<class...Unused>
 	struct front {
+		static_assert(!empty_v, "type vector is empty");
 		using type = at_t<0>;
 	};
 	
-	using front_t = typename front::type;
+	template<class...Unused>
+	using front_t = typename front<Unused...>::type;
 	
+	template<class...Unused>
 	struct back {
-		using type = at_t<(int)sizeof...(Ts)-1>;
+		static_assert(!empty_v, "type vector is empty");
+		using type = at_t<size_v-1>;
 	};
 	
-	using back_t = typename back::type;
+	template<class...Unused>
+	using back_t = typename back<Unused...>::type;
 	
-	template<class T>
+	template<class T, std::size_t = size_v>
 	struct find {
-		template<int index, class... Unused>
-		struct of {
-			static constexpr int value =
+		template<std::size_t index, class... Unused>
+		struct in {
+			static constexpr std::size_t value =
 				std::is_same_v<
 					T, at_t<index>
-				> ? index : of<index+1>::value;
+				> ? index : in<index+1>::value;
 		};
 		
-		template<class...Unused>
-		struct of<sizeof...(Ts), Unused...> {
-			static constexpr int value = -1;
+		template<class... Unused>
+		struct in<size_v-1, Unused...> {
+			static constexpr std::size_t value =
+				std::is_same_v<
+				T, at_t<size_v-1>
+				> ? size_v-1 : not_found_v;
 		};
 
-		static constexpr int value = of<0>::value;
+		static constexpr std::size_t value = in<0>::value;
 	};
 
 	template<class T>
-	static constexpr int find_v = find<T>::value;
+	struct find<T, 0> {
+		static constexpr std::size_t value = not_found_v;
+	};
+
+	template<class T>
+	static constexpr std::size_t find_v = find<T>::value;
 	
+	template<class T>
+	struct contains {
+		static constexpr bool value = find_v<T>!=not_found_v;
+	};
+	
+	template<class T> static constexpr bool contains_v = contains<T>::value;
+
 	template<class T>
 	struct push_front { using type = type_vector<T, Ts...>; };
 
@@ -82,117 +101,99 @@ public:
 	template<class T>
 	using push_back_t = typename push_back<T>::type;
 	
-	template<int beginIn, int endEx>
+	template<std::size_t beginIn, std::size_t endEx, std::size_t = size_v>
 	struct cut {
-		template<int index, class... Result>
-		struct in;
-		
-		template<class Result>
-		struct in<-1, Result> {
-			using type = invalid_t;
+		static_assert(endEx - beginIn <= size_v, "range of bounds");
+		static_assert(endEx - beginIn >= 0, "size of new type vector is negative");
+
+		template<std::size_t index, class Result>
+		struct in {
+			using type =
+				typename in<
+					index+1,
+					typename Result::template push_back_t<
+						at_t<index>
+					>
+				>::type;
 		};
-		
+
 		template<class Result>
-		struct in<sizeof...(Ts)+1, Result> {
-			using type = invalid_t;
-		};
-		
-		template<int index, class Result>
-		struct in<index, Result> {
-			using type = typename
-				std::conditional_t<
-					index == endEx,
-					Result,
-					typename in<index+1, typename Result::template push_back_t<at_t<index>>>::type
-				>;
+		struct in<endEx, Result> {
+			using type = Result;
 		};
 
 		using type = typename in<beginIn, type_vector<>>::type;
 	};
 
-	template<int beginIn, int endEx>
+	template<std::size_t beginIn, std::size_t endEx>
+	struct cut<beginIn, endEx, 0> {
+		static_assert(beginIn == 0 && endEx == 0, "range out of bounds");
+		using type = type_vector<>;
+	};
+
+	template<std::size_t beginIn, std::size_t endEx>
 	using cut_t = typename cut<beginIn, endEx>::type;
 	
-	template<int endEx>
+	template<std::size_t endEx>
 	struct cut_until {
 		using type = cut_t<0, endEx>;
 	};
 
-	template<int endEx>
+	template<std::size_t endEx>
 	using cut_until_t = typename cut_until<endEx>::type;
 
-	template<int beginIn>
+	template<std::size_t beginIn>
 	struct cut_from {
 		using type = cut_t<beginIn, sizeof...(Ts)>;
 	};
 
-	template<int beginIn>
+	template<std::size_t beginIn>
 	using cut_from_t = typename cut_from<beginIn>::type;
 
-	template<class... Ts0> struct append;
-
-	template<class T, class... Ts0>
-	struct append<T, Ts0...> {
-		using type = type_vector<Ts..., T, Ts0...>;
+	template<class... Ts0> struct append {
+		using type = type_vector<Ts..., Ts0...>;
 	};
 	
 	template<class... Ts0>
 	struct append<type_vector<Ts0...>> {
 		using type = type_vector<Ts..., Ts0...>;
 	};
-	
-	template<class... Ts0>
-	struct append<invalid_t, Ts0...> {
-		using type = invalid_t;
-	};
 
 	template<class... Ts0>
 	using append_t = typename append<Ts0...>::type;
 	
-	template<int index, class...Unused>
+	template<std::size_t index, class...Unused>
 	struct erase {
 		using type = typename cut_until_t<index>::template append_t<cut_from_t<index+1>>;
 	};
-	
-	template<class...Unused>
-	struct erase<-1, Unused...> {
-		using type = invalid_t;
-	};
 
-	template<int index>
+	template<std::size_t index>
 	using erase_t = typename erase<index>::type;
 	
 	template<class T>
 	struct erase_first {
+		static_assert(contains_v<T>, "no such type");
 		using type = erase_t<find_v<T>>;
 	};
 
 	template<class T>
 	using erase_first_t = typename erase_first<T>::type;
 	
-	template<class T>
-	struct erase<-1, T> {
-		using type = typename erase<find_v<T>>::type;
-	};
-	
+	template<class...Unused>
 	struct erase_front {
 		using type = erase_t<0>;
 	};
 	
-	using erase_front_t = typename erase_front::type;
+	template<class...Unused>
+	using erase_front_t = typename erase_front<Unused...>::type;
 	
+	template<class...Unused>
 	struct erase_back {
-		using type = erase_t<(int)sizeof...(Ts)-1>;
+		using type = erase_t<size_v-1>;
 	};
 	
-	using erase_back_t = typename erase_back::type;
-	
-	template<class T>
-	struct contains {
-		static constexpr bool value = find_v<T>!=-1;
-	};
-	
-	template<class T> static constexpr bool contains_v = contains<T>::value;
+	template<class...Unused>
+	using erase_back_t = typename erase_back<Unused...>::type;
 };
 
 }
--- test.cpp
+++ test.cpp
@@ -13,10 +13,10 @@ void exec(std::vector<std::string> args) {
 	
 #define SAME(a, b) static_assert(std::is_same_v< a , b >);
 #define EQUALS(a, b) static_assert( a == b );
-	SAME( tl::front_t, int )
-	SAME( tl::back_t, void )
-	SAME( tl::erase_front_t::front_t , bool )
-	SAME( tl3::erase_back_t::back_t , bool )
+	SAME( tl::front_t<>, int )
+	SAME( tl::back_t<>, void )
+	SAME( tl::erase_front_t<>::front_t<> , bool )
+	SAME( tl3::erase_back_t<>::back_t<> , bool )
 	SAME( tl::at_t<0> , int )
 	SAME( tl::at_t<2> , char )
 	EQUALS( tl::find_v<int> , 0 )
@@ -24,7 +24,7 @@ void exec(std::vector<std::string> args) {
 	SAME( tl::cut_until_t<0> , type_vector<> )
 	SAME( tl::cut_until_t<3> , tl3 )
 	SAME( tl::cut_from_t<3> , type_vector<void> )
-	SAME( tl::erase_back_t , tl3 )
+	SAME( tl::erase_back_t<> , tl3 )
 	SAME( tl3::append_t<void> , tl )
 	SAME( tl3::append_t<type_vector<void>> , tl )
 	SAME( tl::erase_t<3> , tl3 )
